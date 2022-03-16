8502468a455b5be230aa488beaeabd4fc75ca2e3
Akeemandela <easytwenty14@gmail.com>
2022-03-15 21:03:38 -0700

    Mu first c programming with functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,23 @@
-#include <main.h>
-#include <unistdio.h>
+#include "main.h"
 /**
- * _putchar - writes the character c to stdout
- *@c: The character to print
- *Return: on success 1.
- *On error, -1 is returned, and errno is set appropriately
+ *main - print out main char by char.
+ *Description:uss the main header file
+ *that contain the functionm _putchar
+ *Return: 0
  */
-int _putchar(char c)
-{
 
-return (write(1, &c, 1));
+int main(void)
+{
+char str[] = "_putchar";
+int i;
+i = 0;
+while (str[i])
+{
+_putchar(str[i]);
+++i;
+}
+_putchar('\n');
+
+
+return (0);
 }
