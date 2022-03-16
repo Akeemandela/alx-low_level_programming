081f62b2f24eed398619bd819378324838f3bf8b
Akeemandela <easytwenty14@gmail.com>
2022-03-16 16:46:38 -0700

    My first c programming with functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - print 10 times the alphabets, then new line..
+*
+*Return: Always 0.
+*/
+
+int print_alphabet_x10(void)
+{
+int count = 0;
+char letter;
+while (count < 10)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+
+_putchar('\n');
+
+count++;
+}
+return (0);
+}
