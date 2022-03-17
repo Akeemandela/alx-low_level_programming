dcdb45ae8940fe62ffb1051249bfa003d9203ead
Akeemandela <easytwenty14@gmail.com>
2022-03-16 18:02:36 -0700

    My first c programming with functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs - Short description
+ *@n: contain values to be subject to abs
+ *Return: returns absolute value of n
+ */
+int _abs(int n)
+{
+if (n > 0)
+{
+n = (-1) * n;
+}
+return (n);
+}
