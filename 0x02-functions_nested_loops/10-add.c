06632024b2f8a96cdb20baab33a033cc43ccba15
Akeemandela <easytwenty14@gmail.com>
2022-03-16 21:29:26 -0700

    My first c programming with functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main"
+/**
+ *add - Adds two numbers and returns the result
+ *@x: number being added
+ *@y: number being added
+ *Return: Always 0
+ */
+int add(int x, int y)
+{
+return (x + y);
+}
