9d837bbd39c8a9ea2eae6c2c64ec16c356f04e74
Akeemandela <easytwenty14@gmail.com>
2022-03-16 17:40:58 -0700

    My first c programming with functions

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *print_sign - prints the sign of a number
+ *@n: contain values to be compared
+ *Return: varies depending on c
+ */
+int print_sign(int n)
+{
+int print_sign = -1;
+if (n > 0)
+{
+print_sign = 1;
+_putchar('+');
+}
+else if (n == 0)
+{
+print_sign = 0;
+_putchar('0');
+}
+else
+{
+_putchar('-');
+}
+return (print_sign);
+}
+
+  
