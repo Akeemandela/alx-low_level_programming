eed0efc4f20af2d0a261c5735ece94318989a60c
Akeemandela <easytwenty14@gmail.com>
2022-03-21 21:26:07 -0700

    My first commit on pointer task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - this function set an integer to the input value
+ *@n: declaration of the integer passed into the function
+ *Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+int *n = 98;
+}
