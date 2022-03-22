a7a13710956b0c37a5f3c6016cf2467e58fb7e9b
Akeemandela <easytwenty14@gmail.com>
2022-03-22 04:22:32 +0100

    My first pointer task in c programming

---
+++ 0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - check the code for any available pointer
+ * @n: declaration of n and parameter for the function reset_to_98
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-My first C programming
+This is my first readme in the pointers and array task
