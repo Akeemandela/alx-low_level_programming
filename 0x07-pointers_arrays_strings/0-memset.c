93068482b91838f2c4d94ebdcada4468a850470e
Akeemandela <easytwenty14@gmail.com>
2022-03-29 04:22:25 +0000

    Pointers commit statement

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *memset - fills memory with a constant bytes.
+ *@s:allocated memory -array of character
+ *@b: char byte
+ *@n:number of spaces to fill
+ *
+ *Return: pointer to memory area s
+ */
+char *memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+*(s + i) = b;
+
+return (s);
+}
+	     
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+My readme filefor 0x07 task
