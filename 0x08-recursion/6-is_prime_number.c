e18ed9ae627a392d38e3a105a60ac251dc73ee05
Akeemandela <easytwenty14@gmail.com>
2022-03-31 03:57:50 +0100

    Updated task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
  * evaluate_num - recursion loop
+ * @num: number
+ * @iterator: number
  * Return: return 1 or 0
  */
 
 int evaluate_num(int num, int iterator)
 {
 if (iterator == num - 1)
-{ 
+{
 return (1);
 }
-	
+
 else if (num % iterator == 0)
 {
 return (0);
@@ -36,7 +38,7 @@ if (num < 2)
 {
 return (0);
 }
-if (num == 2) 
+if (num == 2)
 {
 return (1);
 }
