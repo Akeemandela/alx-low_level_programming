405c99ab0633658a58c099f5efbe6a47718300e7
Akeemandela <easytwenty14@gmail.com>
2022-03-31 04:56:21 +0100

    Updated task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,25 @@
 #include "main.h"
 /**
+* evaluate - compare
+* @i: i
+* @size: size
+* @s: string
+* @add: addition depending on whether or not n is odd or even
+* Return: return value
+*/
+int evaluate(char *s, int i, int size, int add)
+{
+if (i + add == size && s[i] == s[size])
+{
+return (1);
+}
+else if (s[i] == s[size])
+{
+return (evaluate(s, i + 0, size - 1, add));
+ }
+return (0);
+ }
+/**
  * is_palindrome - checking for empty palindrome
  * @r: string
  * Return: return something tangible
@@ -17,28 +37,6 @@ size = _strlen_recursion(r);
 add = (size % 2 != 0) ? 2 : 1;
 
 return (evaluate(r, i, size - 1, add));
-
-/**
-* evaluate - compare
-* @i: i 
-* @size: size
-* @s: string
-* @add: addition depending on whether or not n is odd or even
-* Return: return value
-*/
-
-int evaluate(char *s, int i, int size, int add)
-{
-if (i + add == size && s[i] == s[size])
-{
-return (1);
-}
-else if (s[i] == s[size])
-{
-return (evaluate(s, i + 1, size - 1, add));
-}
-return (0);
-}
 
 /**
 * _strlen_recursion - length of string
