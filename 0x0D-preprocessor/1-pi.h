18ea4c74c17a3c894562929544ef27e6f4ae0c7d
Akeemandela <easytwenty14@gmail.com>
2022-04-11 15:00:45 +0100

    My first preprocessor task

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 #ifndef PI 
 #define PI 3.14159265359
 #endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print file name
+ * Return: Always 0
+ */
+
+int main(void)
+{
+printf("%s\n", _FILE_);
+return (0);
+}
