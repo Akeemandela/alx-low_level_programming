df67c07143acd3d781b16ebb2c0d5a1a6df625b2
Akeemandela <easytwenty14@gmail.com>
2022-04-12 12:37:25 +0100

    My Preprocessor Task

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-#define ABS(x) ((x < 0) ? (x) * (-1) : (x));
+#ifndef ABS_FILE
+#define ABS_FILE
+#define ABS(x) ((x < 0 ? -1 * (x) i: (x));
 #endif
