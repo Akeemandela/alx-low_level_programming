fb2d0c08677193185769a83810521a3b5a672f5e
Akeemandela <easytwenty14@gmail.com>
2022-04-13 14:45:23 +0100

    My first function pointer task

--- 0x0F-function_pointers/o-print_name.c
+++ 0x0F-function_pointers/o-print_name.c
@@ -2,11 +2,11 @@
 #include "function_pointers.h"
 
 /**
- * print_name - function that prints a name.
- *@name: name
- *@f: callback function
- * Description: function that prints a name
- */
+* print_name - function that prints a name.
+*@name: name
+*@f: callback function
+* Description: function that prints a name
+*/
 
 void print_name(char *name, void (*f)(char *))
 
