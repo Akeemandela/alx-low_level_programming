cfff8007228da22a99015676c1b19261f4502fb0
Akeemandela <easytwenty14@gmail.com>
2022-05-21 10:25:42 +0100

    updated main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,20 +1,14 @@
-<<<<<<< HEAD
-#ifdef MAIN_H
-#ifdefine MAIN_H
-
-=======
-y#ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
->>>>>>> df67c07143acd3d781b16ebb2c0d5a1a6df625b2
+
 #include <stdio.h>
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-<<<<<<< HEAD
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
-=======
-
->>>>>>> df67c07143acd3d781b16ebb2c0d5a1a6df625b2
 
 #endif /* MAIN_H */
+
+
+
