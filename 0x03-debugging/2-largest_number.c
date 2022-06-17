a143dd5434ce99cde72dd580db67e2f684458dc8
Akeemandela <easytwenty14@gmail.com>
2022-06-17 04:28:59 +0100

    Updated debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,35 +1,29 @@
-
 #include "main.h"
-/**
- *largest_number - return the largest of 3 numbers
- *@a:first integer
- *@b:second integer
- *@c:third integer
- *Description: the function finds the largest of three numbers
- *Return: largest number
- */
-int main(void)
-{
-int large = largerst_number(a, b, c);
 
-return (0);
-}
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
 int largest_number(int a, int b, int c)
 {
 int largest;
-if (a > b && a > c)
+
+if (a >= b && a >= c)
 {
 largest = a;
 }
-else if (b > a && b > c)
+else if (b >= a && b >= c)
 {
 largest = b;
 }
-else if (c > a && c > b)
+else
 {
 largest = c;
 }
-return (largest);
-}
 
+return (largest);
 
