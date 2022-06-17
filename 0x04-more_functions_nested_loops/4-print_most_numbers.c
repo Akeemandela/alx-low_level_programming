7bc48b2579be5fdf0293555a47f6c03428eaa46c
Akeemandela <easytwenty14@gmail.com>
2022-06-17 05:33:48 +0100

    Updated quick fixes

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,23 +1,18 @@
-#include "maln.h"
+#include "main.h"
 /**
- * print_most_numbers - print numbers
- *
- * Return: Always 0.
+ * print_most_numbers - prints the numbers from 0 to 9,
+ * except 2 and 4 followed by a new line
  */
 void print_most_numbers(void)
 {
+int i;
 
-int i; /*Declaring statements*/
-
-i = 0;
-while (i < 10)/*Start While*/
-	{
-if (i != 2 && i != 4) /* No print 2 and 4*/
+for (i = 0; i < 10; i++)
 {
-_putchar (i + '0'); /*print value i*/
+if (i != 2 && i != 4)
+{
+_putchar(i + '0');
 }
-i++;
 }
-
-_putchar ('\n'); /*New line*/
+_putchar('\n');
 }
