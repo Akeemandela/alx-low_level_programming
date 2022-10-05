06a18176f8813618987327f762b9af91e622b204
Akeemandela <easytwenty14@gmail.com>
2022-10-05 14:01:22 +0100

    This is a search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,13 +13,13 @@ int linear_search(int *array, size_t size, int value)
 size_t index;
 
 if (array == NULL)
-	return (-1);
+return (-1);
 
 for (index = 0; index < size; index++)
 {
-	printf("Value checked array[%li] = [%i]\n", index, array[index]);
-	if (array[index] == value)
-		return (index);
+printf("Value checked array[%li] = [%i]\n", index, array[index]);
+if (array[index] == value)
+return (index);
 }
 return (-1);
 }
