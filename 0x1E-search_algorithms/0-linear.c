08cb165250578a90642c99d7f65f3797c9f27931
Akeemandela <easytwenty14@gmail.com>
2022-10-05 13:55:04 +0100

    This is a search algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - linear search implementation
+ * @array: string
+ * @size: size_t
+ * @value: value
+ * Return: int, index of the value
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+size_t index;
+
+if (array == NULL)
+	return (-1);
+
+for (index = 0; index < size; index++)
+{
+	printf("Value checked array[%li] = [%i]\n", index, array[index]);
+	if (array[index] == value)
+		return (index);
+}
+return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,2 @@
+This algorithm uses the search algorithms to search through 
+the list of a given data. It is meant to sharpen my datastructure and algorithm strength.
