18356f609b1a1dfbf30e031871103846272b20ec
Akeemandela <easytwenty14@gmail.com>
2022-12-09 15:13:53 +0100

    Updated search algorithms

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,56 +1,61 @@
 #include "search_algos.h"
-/**
- * binary_search - binary search implementation
- * @array: string
- * @size: size_t
- * @value: value
- * Return: int, index of the value
- */
 
+/**
+ * recursive_search - searches for a value in an array of
+ * integers using the Binary search algorithm
+ *
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int recursive_search(int *array, size_t size, int value)
+{
+	size_t half = size / 2;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+		return ((int)half);
+
+	if (value < array[half])
+		return (recursive_search(array, half, value));
+
+	half++;
+
+	return (recursive_search(array + half, size - half, value) + half);
+}
+
+/**
+ * binary_search - calls to binary_search to return
+ * the index of the number
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
 int binary_search(int *array, size_t size, int value)
 {
-size_t index;
-int l_search = 0;
-int r_search = size;
-int mid_search;
-if (array == NULL)
-return (-1);
+	int index;
 
-for (index = 0; l_search < r_search; index++)
-{
-printf("Searching in array: ");
-print_array(array, l_search, r_search);
-mid_search = (l_search + r_search) / 2;
+	index = recursive_search(array, size, value);
 
-if (array[mid_search] < value)
-l_search = mid_search + 1;
-else if (array[mid_search] > value)
-r_search = mid_search - 1;
-else
-return (mid_search);
-}
-return (-1);
-}
+	if (index >= 0 && array[index] != value)
+		return (-1);
 
-
-/**
- * print_array - print `n` elements of an array of integers
- * @a: int array pointer to print
- * @i: int, start index
- * @j: int, end index
- * Description: Numbers must be separated by comma and space.
- * Numbers should be displayed in the same order they are stored in array.
- * You can only use _putchar to print.
- */
-
-void print_array(int *a, int i, int j)
-{
-
-for (; i < j; i++)
-{
-printf("%d", a[i]);
-if (i < j - 1)
-printf(", ");
-}
-printf("\n");
+	return (index);
 }
