1aacffb9667faab9c41a31163db480dc51a2de0e
Akeemandela <easytwenty14@gmail.com>
2022-12-14 03:01:59 -0800

    Updated variables, if_else_while in C programming

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,33 +1,30 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
+
 /**
- * main - Entry point
- *Dcription: Print statements based on the last digit of the numbers.
- *Return: Always 0 (Success)
-*/
+ * main - Entry
+ *
+ * Description: Prints the last digit of n and determines if it is
+ * greater than 5, equal to 0, or less than 6 but not equal to 0
+ *
+ * Return: returns 0 (Success)
+ */
+
 int main(void)
 {
-int n;
-int m;
+	int n;
+	int m;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-m = n % 10;
-  
-if (m > 5)
-{
-printf("Last digit of %i is %i and is greater than 5\n", n, m);
-}
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	m = n % 10;
 
-else if (m == 0)
-{
-printf("Last digit of %i is %i and is 0\n", n, m);
-}
-
-else
-{
-printf("Last digit of %i is %i and is greater than 6 and not 0\n" n, m);
-}
-return (0);
+	if (m > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, m);
+	else if (m == 0)
+		printf("Last digit of %i is %i and is 0\n", n, m);
+	else
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, m);
+	return (0);
 }
